9025cc5b45d3e99ddf66a3e3da8ae96b961c5e5b
Stanislav Nemchenko <stas.buka@gmail.com>
2026-02-16 23:26:59 +0200

    feat: Initialize main application entry point with React, ReactDOM, and React Router setup.

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <BrowserRouter>
+        <BrowserRouter basename="/psychologists-services">
             <App />
         </BrowserRouter>
     </React.StrictMode>,
